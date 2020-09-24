dc67fd132f4c76ba574514516ec3d67b2664ce61
seasons3 <lcli_3@hotmail.com>
2020-09-24 12:38:01 -0400

    test pipleline

--- hello-world/app.js
+++ hello-world/app.js
@@ -5,7 +5,7 @@ exports.lambdaHandler = async (event, context) => {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'hello world',
+            message: 'hello world!!',
         })
     };
 
